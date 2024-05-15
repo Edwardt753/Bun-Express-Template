1bee243c574d3df1e76375267619e86a10c11790
Edwart753 <edwardthomas753@gmail.com>
2024-05-15 16:54:39 +0700

    test commit

--- index.ts
+++ index.ts
@@ -6,6 +6,9 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/post", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
